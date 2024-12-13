22221c58e0c0f83edaf4108ff56e1dfae03f8b6d
Egg-Fish <eugeniomanansala@gmail.com>
2024-12-13 17:44:25 +0800

    feat: change individual V registers into array

--- src/CHIP-8.c
+++ src/CHIP-8.c
@@ -9,22 +9,7 @@
 
 struct c8_machine {
     struct {
-        uint8_t V0;
-        uint8_t V1;
-        uint8_t V2;
-        uint8_t V3;
-        uint8_t V4;
-        uint8_t V5;
-        uint8_t V6;
-        uint8_t V7;
-        uint8_t V8;
-        uint8_t V9;
-        uint8_t VA;
-        uint8_t VB;
-        uint8_t VC;
-        uint8_t VD;
-        uint8_t VE;
-        uint8_t VF;
+        uint8_t V[16];
 
         uint16_t I;
         uint16_t PC;
